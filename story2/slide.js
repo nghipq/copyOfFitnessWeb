9120e1c588ad2712c28b8f3bdd1e065440d8c5b7
nghipq <nghipqce140179@fpt.edu.vn>
2019-04-21 20:32:39 +0700

    update story.js

--- story2/slide.js
+++ story2/slide.js
@@ -2,7 +2,8 @@ var index = 0;
 changeClass(index);
 
 function changeClass(index) {
-    var buttons = document.getElementsByClassName("button")
+    var buttons = document.getElementsByClassName("button");
+    var slides = document.getElementsByClassName("story");
     setInterval(function() {
         var i;
         index += 1;
@@ -10,11 +11,13 @@ function changeClass(index) {
             index = 1
         };
         buttons[index - 1].classList.add("active");
+        slides[index - 1].classList.add("screen");
         i = index - 2;
         if(i < 0) {
             i = buttons.length - 1
         }
-        buttons[i].classList.remove("active")
+        buttons[i].classList.remove("active");
+        slides[i].classList.remove("screen");
     }, 6000)
 }
 
